773e3bc81ca0f72bcd4df6d5bce4e44ba8e1940c
Rob B <robinnep@gmail.com>
2017-03-28 14:04:18 -0500

    change paths

--- pull.bat
+++ pull.bat
@@ -1,3 +1,3 @@
 @echo on
-"C:\Users\e115487\AppData\Local\Continuum\Anaconda3 4.2\python.exe" "G:\RevNetworkAnalysis\Economic & Industry Analysis\Projects\FRED data\20170306_Fred pull.py"
+"C:\...\python.exe" "C:\...Fred pull.py"
 pause
